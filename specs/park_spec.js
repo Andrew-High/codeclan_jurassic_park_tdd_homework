bc3c7621c7d086894bff86570008960acb09f896
Andrew High <andrewhigh4@hotmail.com>
2021-05-04 19:31:43 +0100

    11 test passing, skipped one

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -67,7 +67,13 @@ describe('Park', function() {
     assert.deepStrictEqual(actual, 80)
   });
 
-  it('should be able to calculate the total number of visitors per year');
+  it('should be able to calculate the total number of visitors per year', function(){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    const actual = 365 * park.findTotalVisitorsPerDay();
+    assert.deepStrictEqual(actual, 29200)
+  });
 
   it('should be able to calculate total revenue for one year');
 
